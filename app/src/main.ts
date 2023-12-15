3349e5865b77144f5899e9a2e66ab1b853748f53
cauta <trongcauhcmus@gmail.com>
2023-12-15 17:42:15 +0700

    add swagger auth

--- app/src/main.ts
+++ app/src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     .setTitle('Blockchain Webhook')
     .setDescription('Blockchain transaction and address monitoring with webhook')
     .setVersion('1.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT', }, 'JWT',)
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
