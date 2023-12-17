0b3c157062dad4924b30ed1b1b5b3d7aa3ebbb32
cauta <trongcauhcmus@gmail.com>
2023-12-17 11:34:38 +0700

    fix jwt auth guard

--- app/src/modules/auth/guards/jtw-auth.guard.ts
+++ app/src/modules/auth/guards/jtw-auth.guard.ts
@@ -4,36 +4,4 @@ import { jwtConstants } from '../constants';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-    constructor(private jwtService: JwtService) {
-        super();
-    }
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
-    if (!token) {
-      throw new UnauthorizedException();
-    }
-    try {
-      const payload = await this.jwtService.verifyAsync(
-        token,
-        {
-          secret: jwtConstants.secret
-        }
-      );
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
-      request['user'] = payload;
-      
-    } catch {
-      throw new UnauthorizedException();
-    }
-    return true;
-  }
-
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
