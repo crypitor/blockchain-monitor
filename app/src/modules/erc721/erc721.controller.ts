f4ef5d1877b4a746f062d87b047716ebe1760c17
cauta <trongcauhcmus@gmail.com>
2023-12-19 10:57:32 +0700

    update error handler in controller

--- app/src/modules/erc721/erc721.controller.ts
+++ app/src/modules/erc721/erc721.controller.ts
@@ -4,74 +4,34 @@ import {
   Body,
   UsePipes,
   BadRequestException,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateERC721TokenInfoDto } from './dto/create-erc721-tokeninfo.dto';
 import { ERC721Service } from './erc721.service';
-import { Model } from 'mongoose';
 import { CreateERC721ValidationPipe } from './erc721.pipe';
 import { ERC721 } from './schemas/erc721.schema';
 import { DeleteERC721TokenInfoDto } from './dto/delete-erc721-tokeninfo.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
+@ApiTags('ERC721')
+@ApiBearerAuth('JWT')
 @Controller('erc721')
 export class ERC721Controller {
   constructor(private ERC721Service: ERC721Service) { }
 
   @Post('/create')
+  // @UseGuards(JwtAuthGuard)
   @UsePipes(new CreateERC721ValidationPipe())
   async create(@Body() erc721: CreateERC721TokenInfoDto) {
-    try {
-      const result: InstanceType<typeof ERC721> = await this.ERC721Service.create(
-        erc721,
-      );
-      return result;
-    } catch (error) {
-      if (error.hasOwnProperty('code')) {
-        if (error.name === 'MongoServerError' && error.code === 11000) {
-          throw new BadRequestException({
-            error: {
-              address: {
-                notUnique: 'address is already in use by a different id',
-              },
-            },
-          });
-        }
-      }
-
-      throw new BadRequestException(
-        {
-          error: error,
-        },
-        'Bad request',
-      );
-    }
+    const result: InstanceType<typeof ERC721> = await this.ERC721Service.create(erc721);
+    return result;
   }
 
   @Post('/remove')
+  // @UseGuards(JwtAuthGuard)
   @UsePipes(new CreateERC721ValidationPipe())
   async remove(@Body() erc721: DeleteERC721TokenInfoDto) {
-    try {
-      await this.ERC721Service.deleteOne(
-        erc721.token_address,
-      );
-    } catch (error) {
-      if (error.hasOwnProperty('code')) {
-        if (error.name === 'MongoServerError' && error.code === 11000) {
-          throw new BadRequestException({
-            error: {
-              address: {
-                notUnique: 'address is already in use by a different id',
-              },
-            },
-          });
-        }
-      }
-
-      throw new BadRequestException(
-        {
-          error: error,
-        },
-        'Bad request',
-      );
-    }
+    await this.ERC721Service.deleteOne(erc721.token_address);
   }
 }
--- app/src/modules/wallet/wallet.controller.ts
+++ app/src/modules/wallet/wallet.controller.ts
@@ -4,74 +4,34 @@ import {
   Body,
   UsePipes,
   BadRequestException,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateWalletWebhookDto } from './dto/create-wallet.dto';
 import { WalletService } from './wallet.service';
-import { Model } from 'mongoose';
 import { CreateWalletValidationPipe } from './wallet.pipe';
-import { Wallet, WalletSchema } from './schemas/wallet.schema';
+import { Wallet } from './schemas/wallet.schema';
 import { DeleteWalletDto } from './dto/delete-wallet.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Wallet')
+@ApiBearerAuth('JWT')
 @Controller('wallet')
 export class WalletController {
   constructor(private WalletService: WalletService) { }
 
   @Post('/create')
+  // @UseGuards(JwtAuthGuard)
   @UsePipes(new CreateWalletValidationPipe())
   async create(@Body() wallet: CreateWalletWebhookDto) {
-    try {
-      const result: InstanceType<typeof Wallet> = await this.WalletService.create(
-        wallet,
-      );
-      return result;
-    } catch (error) {
-      if (error.hasOwnProperty('code')) {
-        if (error.name === 'MongoServerError' && error.code === 11000) {
-          throw new BadRequestException({
-            error: {
-              address: {
-                notUnique: 'address is already in use by a different id',
-              },
-            },
-          });
-        }
-      }
-
-      throw new BadRequestException(
-        {
-          error: error,
-        },
-        'Bad request',
-      );
-    }
+    const result: InstanceType<typeof Wallet> = await this.WalletService.create(wallet);
+    return result;
   }
 
   @Post('/remove')
+  // @UseGuards(JwtAuthGuard)
   @UsePipes(new CreateWalletValidationPipe())
   async remove(@Body() wallet: DeleteWalletDto) {
-    try {
-      await this.WalletService.deleteOne(
-        wallet.address,
-      );
-    } catch (error) {
-      if (error.hasOwnProperty('code')) {
-        if (error.name === 'MongoServerError' && error.code === 11000) {
-          throw new BadRequestException({
-            error: {
-              address: {
-                notUnique: 'address is already in use by a different id',
-              },
-            },
-          });
-        }
-      }
-
-      throw new BadRequestException(
-        {
-          error: error,
-        },
-        'Bad request',
-      );
-    }
+    await this.WalletService.deleteOne(wallet.address);
   }
 }
