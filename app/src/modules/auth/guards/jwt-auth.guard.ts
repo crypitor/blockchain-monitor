4567965a55caae562bd798bc4a9f48f3d2b5683c
cauta <trongcauhcmus@gmail.com>
2024-03-11 10:58:50 +0700

    update user api and auth

--- app/src/modules/auth/guards/jwt-auth.guard.ts
+++ app/src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,5 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { jwtConstants } from '../constants';
-import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
