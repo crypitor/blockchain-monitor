c2902a2cd07f0f0bdbefff7a525590a63d7aae90
cauta <trongcauhcmus@gmail.com>
2024-03-11 12:27:29 +0700

    update eth webhook service

--- app/src/modules/webhooks/ethereum/eth.webhook.service.ts
+++ app/src/modules/webhooks/ethereum/eth.webhook.service.ts
@@ -1,10 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { CreateEthWebhookDto } from './dto/eth.create-webhook.dto';
 import { EthWebhook } from './schemas/eth.webhook.schema';
+import { v4 as uuidv4 } from 'uuid';
+import { Log, ethers } from 'ethers';
+import { WebhookDeliveryDto } from './dto/eth.webhook.dto';
 
 @Injectable()
 export class EthWebhookService {
+  private readonly logger = new Logger(EthWebhookService.name);
   constructor(
     @Inject('ETH_WEBHOOK_MODEL')
     private readonly ethWebhookModel: Model<EthWebhook>,
@@ -14,7 +18,11 @@ export class EthWebhookService {
     user: any,
     createEthWebhookDto: CreateEthWebhookDto,
   ): Promise<EthWebhook> {
-    const createdEthWebhook = new this.ethWebhookModel(createEthWebhookDto);
+    const createdEthWebhook = new this.ethWebhookModel({
+      ...createEthWebhookDto,
+      userId: user.userId,
+      webhookId: uuidv4(),
+    });
     try {
       return await createdEthWebhook.save();
     } catch (error) {
@@ -56,5 +64,71 @@ export class EthWebhookService {
     } catch (error) {
       throw error;
     }
+  }
+
+  async handleErc20Transfer(event: Log, confirm: boolean) {
+    // Extract relevant information from the event
+    const contractAddress = ethers.getAddress(event.address).toLowerCase();
+    const fromAddress = ethers
+      .getAddress(event.topics[1].substring(26))
+      .toLowerCase();
+    const toAddress = ethers
+      .getAddress(event.topics[2].substring(26))
+      .toLowerCase();
+    const value = ethers.toBigInt(event.data).toString();
+
+    // handle from wallet
+    const fromWallet = await this.findOne(fromAddress);
+    if (fromWallet) {
+      // @todo change webhookDTO to general dto
+      const body = new WebhookDeliveryDto(
+        event,
+        1,
+        fromWallet.webhookId,
+        'out',
+        confirm,
+        value,
+      );
+
+      // @todo send message to webhook module to deliver for user
+      await this.sendMessage(fromWallet, body);
+
+      this.logger.debug(
+        'Confirmed:' + confirm + ' ERC20 transfer OUT: ' + JSON.stringify(body),
+      );
+    }
+
+    // handle to wallet
+    const toWallet = await this.findOne(toAddress);
+    if (toWallet) {
+      // @todo change webhookDTO to general dto
+      const body = new WebhookDeliveryDto(
+        event,
+        1,
+        toWallet.webhookId,
+        'out',
+        confirm,
+        value,
+      );
+
+      // @todo send message to webhook module to deliver for user
+      await this.sendMessage(toWallet, body);
+
+      this.logger.debug(
+        'Confirmed:' + confirm + ' ERC20 transfer OUT: ' + JSON.stringify(body),
+      );
+    }
+  }
+
+  async sendMessage(wallet: EthWebhook, body: WebhookDeliveryDto) {
+    // @todo handle all notification method
+    // @todo handle reesponse fail
+    const response = await fetch(wallet.notificationMethods[0].url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
   }
 }
