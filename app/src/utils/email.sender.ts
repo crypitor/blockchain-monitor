c66855139d0af726b40af7a3bb48c1508abd65ba
cauta <trongcauhcmus@gmail.com>
2024-03-18 14:55:11 +0700

    fix mail service

--- app/src/utils/email.sender.ts
+++ app/src/utils/email.sender.ts
@@ -1,20 +1,17 @@
 import * as nodemailer from 'nodemailer';
 
-const SMTP_CONFIG = {
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
-};
-
-const transporter = nodemailer.createTransport(SMTP_CONFIG);
-const sender = process.env.SMTP_SERNDER;
-
 export const sendEmail = async (to: string, subject: string, html: string) => {
+  const SMTP_CONFIG = {
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  };
+  const transporter = nodemailer.createTransport(SMTP_CONFIG);
   return await transporter.sendMail({
-    from: sender,
+    from: process.env.SMTP_SERNDER,
     to,
     subject,
     html,
