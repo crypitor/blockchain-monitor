5367475530e258b45f06438d90f8adbb81dca591
cauta <trongcauhcmus@gmail.com>
2024-03-18 13:58:15 +0700

    update config db url

--- app/src/database/database.providers.ts
+++ app/src/database/database.providers.ts
@@ -4,7 +4,9 @@ export const Database = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): mongoose.Connection =>
-      mongoose.createConnection(process.env.DB_URL),
+      mongoose.createConnection(
+        `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE_NAME}?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0`,
+      ),
   },
   // {
   //   provide: 'DATABASE_USER_CONNECTION',
