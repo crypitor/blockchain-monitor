2da74e888f1b4a3201216a99f873424b30ab30dd
cauta <trongcauhcmus@gmail.com>
2024-03-25 15:50:21 +0700

    remove unused module for scan module

--- app/src/apps/scanner/scanner.module.ts
+++ app/src/apps/scanner/scanner.module.ts
@@ -4,12 +4,7 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from 'src/database/database.module';
 import { GlobalModule } from 'src/global/global.module';
 import { GlobalService } from 'src/global/global.service';
-import { AuthModule } from 'src/modules/auth/auth.module';
 import { BlockSyncModule } from 'src/modules/blocksync/blocksync.module';
-import { ERC721Module } from 'src/modules/erc721/erc721.module';
-import { NftModule } from 'src/modules/nft/nft.module';
-import { UsersModule } from 'src/modules/users/users.module';
-import { WalletModule } from 'src/modules/wallet/wallet.module';
 import { EthMonitorModule } from 'src/modules/webhooks/ethereum/eth.monitor.module';
 import { EthereumWorker } from 'src/modules/webhooks/ethereum/worker/evm.worker';
 
@@ -21,14 +16,9 @@ import { EthereumWorker } from 'src/modules/webhooks/ethereum/worker/evm.worker'
       expandVariables: true,
     }),
     DatabaseModule,
-    UsersModule,
-    AuthModule,
     GlobalModule,
-    WalletModule,
     BlockSyncModule,
-    ERC721Module,
     ScheduleModule.forRoot(),
-    NftModule,
     EthMonitorModule,
   ],
   providers: [GlobalService, EthereumWorker],
