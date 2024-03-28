4130403678533bcb40cb5dc70947ff2ce4be40bd
cauta <trongcauhcmus@gmail.com>
2024-03-28 15:08:59 +0700

    config worker consumer microservice

--- app/apps/worker-service/src/main.ts
+++ app/apps/worker-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { WorkerServiceModule } from './worker-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(WorkerServiceModule);
-  await app.listen(3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    WorkerServiceModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: process.env.KAFKA_BROKERS.split(','),
+        },
+        consumer: {
+          groupId: 'worker-consumer',
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
