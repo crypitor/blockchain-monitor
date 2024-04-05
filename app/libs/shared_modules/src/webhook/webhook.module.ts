645b159c1d1974d33c7a483295dac660eee9bf17
cauta <trongcauhcmus@gmail.com>
2024-04-05 12:18:13 +0700

    export webhook service

--- app/libs/shared_modules/src/webhook/webhook.module.ts
+++ app/libs/shared_modules/src/webhook/webhook.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
 
 @Module({
+  imports: [],
+  exports: [WebhookService],
   providers: [WebhookService],
 })
 export class WebhookModule {}
