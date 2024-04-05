c618e6030270f50eac7aa372e2d78fc9e2f278d8
cauta <trongcauhcmus@gmail.com>
2024-04-05 12:18:35 +0700

    import webhook module to monitor module

--- app/apps/onebox/src/modules/monitor/monitor.module.ts
+++ app/apps/onebox/src/modules/monitor/monitor.module.ts
@@ -3,10 +3,11 @@ import { ProjectModule as ProjectModelModule } from '@app/shared_modules/project
 import { Module } from '@nestjs/common';
 import { MonitorController } from './monitor.controller';
 import { MonitorService } from './monitor.service';
+import { WebhookModule } from '@app/shared_modules/webhook/webhook.module';
 @Module({
   controllers: [MonitorController],
   providers: [MonitorService],
   exports: [MonitorService],
-  imports: [MonitorModelModule, ProjectModelModule],
+  imports: [MonitorModelModule, ProjectModelModule, WebhookModule],
 })
 export class MonitorModule {}
