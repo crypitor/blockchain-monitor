02b18e620e7630bcbb24df46dd386049092c7748
cauta <trongcauhcmus@gmail.com>
2024-04-05 09:34:10 +0700

    remove old module

--- app/apps/onebox/src/modules/webhooks/ethereum/dto/eth.delete-monitor.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class DeleteMonitorDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  monitorId: string;
-}
