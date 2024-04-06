e434bf4bc516186c1640d26e4d5864db47ce6fb1
cauta <trongcauhcmus@gmail.com>
2024-04-06 10:36:04 +0700

    fix supported chain

---
+++ app/libs/utils/src/supportedChain.util.ts
@@ -0,0 +1,64 @@
+export class SupportedChain {
+  static ETH = {
+    id: 1,
+    idHex: '0x1',
+    name: 'ETH',
+    confirmationBlock: 12,
+  };
+
+  static BSC = {
+    id: 56,
+    idHex: '0x38',
+    name: 'BSC',
+    confirmationBlock: 12,
+  };
+
+  static POLYGON = {
+    id: 137,
+    idHex: '0x89',
+    name: 'POLYGON',
+    confirmationBlock: 100,
+  };
+
+  static AVALANCHE = {
+    id: 43114,
+    idHex: '0xa86a',
+    name: 'AVALANCHE',
+    confirmationBlock: 100,
+  };
+
+  static FANTOM = {
+    id: 250,
+    idHex: '0xfa',
+    name: 'FANTOM',
+    confirmationBlock: 100,
+  };
+
+  static ARBITRUM = {
+    id: 42161,
+    idHex: '0xa4b1',
+    name: 'ARBITRUM',
+    confirmationBlock: 100,
+  };
+
+  static OPTIMISM = {
+    id: 10,
+    idHex: '0xa',
+    name: 'OPTIMISM',
+    confirmationBlock: 100,
+  };
+
+  static BASE = {
+    id: 8453,
+    idHex: '0x2105',
+    name: 'BASE',
+    confirmationBlock: 100,
+  };
+
+  static GNOSIS = {
+    id: 100,
+    idHex: '0x64',
+    name: 'GNOSIS',
+    confirmationBlock: 100,
+  };
+}
