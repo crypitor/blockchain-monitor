e11ad271886fff9cf6b3505af5c2b315d347d135
cauta <trongcauhcmus@gmail.com>
2024-04-07 11:07:44 +0700

    start listen to polling block from onebox for worker

--- app/apps/worker-service/src/worker-service.controller.ts
+++ app/apps/worker-service/src/worker-service.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { WorkerServiceService } from './worker-service.service';
+import { EventPattern } from '@nestjs/microservices';
+import { TopicName } from '@app/utils/topicUtils';
 
 @Controller()
 export class WorkerServiceController {
@@ -8,5 +10,15 @@ export class WorkerServiceController {
   @Get()
   getHello(): string {
     return this.workerServiceService.getHello();
+  }
+
+  @EventPattern(TopicName.ETH_DETECTED_BLOCK)
+  async ethDetectBlock(data: any) {
+    this.workerServiceService.ethHandleDetectedBlock(data);
+  }
+
+  @EventPattern(TopicName.ETH_CONFIRMED_BLOCK)
+  async ethConfirmBlock(data: any) {
+    this.workerServiceService.ethHandleConfirmedBlock(data);
   }
 }
