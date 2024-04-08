7794e9d704a782a4814c8913d7a9845d24cd9dc4
thong.nguyen5 <thong.nguyen5@mservice.com.vn>
2024-04-08 21:01:09 +0700

    Auto create project when user register

--- app/apps/onebox/src/modules/users/users.module.ts
+++ app/apps/onebox/src/modules/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersProviders } from './users.providers';
 import { UsersController } from './users.controller';
 import { DatabaseModule } from '@app/database';
+import { ProjectModule } from '../project/project.module';
 @Module({
   controllers: [UsersController],
   providers: [UsersService, ...UsersProviders],
   exports: [UsersService],
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ProjectModule],
 })
 export class UsersModule {}
