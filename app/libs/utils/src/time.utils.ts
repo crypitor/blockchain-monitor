e90d522a30fd2f3f75800625d10e71fbbffeae90
thong.nguyen5 <thong.nguyen5@mservice.com.vn>
2024-04-13 21:19:56 +0700

    Update async await and add time measure

---
+++ app/libs/utils/src/time.utils.ts
@@ -0,0 +1,12 @@
+import { Logger } from '@nestjs/common';
+
+export async function measureTime<T>(
+  label: string,
+  fn: () => Promise<T>,
+): Promise<T> {
+  const start = performance.now();
+  const result = await fn();
+  const end = performance.now();
+  Logger.log(`Latency for [${label}]: ${(end - start).toFixed(3)}ms`);
+  return result;
+}
