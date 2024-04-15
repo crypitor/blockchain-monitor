2ee8d2f2a2c37073b1d260d19f93009d5ee62fce
cauta <trongcauhcmus@gmail.com>
2024-04-15 16:33:24 +0700

    remove old module

--- app/apps/worker-service/src/blocksync/dto/delete-blocksync.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsLowercase, IsNotEmpty, IsString, MaxLength } from 'class-validator';
-
-export class DeleteBlockSyncDto {
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(42)
-  @IsLowercase()
-  rpcUrl: string;
-}
