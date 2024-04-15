4e19b57f4cd09194eea182bfe3e533a0d724586b
thong.nguyen5 <thong.nguyen5@mservice.com.vn>
2024-04-15 21:23:32 +0700

    CRYP-48 Add filter for monitor search

--- app/libs/shared_modules/src/monitor/repositories/monitor.repository.ts
+++ app/libs/shared_modules/src/monitor/repositories/monitor.repository.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ListMonitorDto } from 'apps/onebox/src/modules/monitor/dto/monitor.dto';
 import { Model } from 'mongoose';
 import { Monitor } from '../schemas/monitor.schema';
 
@@ -18,6 +19,30 @@ export class MonitorRepository {
 
   async listMonitorsByProject(projectId: string): Promise<Monitor[]> {
     return this.monitorModel.find({ projectId: projectId });
+  }
+
+  async listMonitors(filter: ListMonitorDto): Promise<Monitor[]> {
+    const match = {
+      projectId: filter.projectId,
+    };
+    if (filter.network) {
+      match['network'] = filter.network;
+    }
+
+    if (filter.search && filter.search !== '') {
+      if (filter.search.startsWith('mo_')) {
+        match['monitorId'] = filter.search;
+      } else {
+        match['name'] = { $regex: filter.search, $options: 'i' };
+      }
+    }
+
+    return this.monitorModel
+      .find(match)
+      .sort({ _id: -1 })
+      .limit(filter.limit)
+      .skip(filter.offset)
+      .exec();
   }
 
   async deleteMonitor(monitorId: string): Promise<boolean> {
