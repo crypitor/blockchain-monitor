049fa99cf2ee7387d3f9735562a94b2a5e928b56
cauta <trongcauhcmus@gmail.com>
2024-05-03 14:52:05 +0700

    remove unused topic

--- app/apps/monitor-service/src/monitor-service.controller.ts
+++ app/apps/monitor-service/src/monitor-service.controller.ts
@@ -2,8 +2,8 @@ import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 @Controller()
 export class MonitorServiceController {
-  @EventPattern('blockchain-event')
-  getHello(data: any) {
-    console.log('data', data);
-  }
+  // @EventPattern('blockchain-event')
+  // getHello(data: any) {
+  //   console.log('data', data);
+  // }
 }
