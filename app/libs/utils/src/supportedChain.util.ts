e21dea93f9a45ee4d549a59a6ceee2501aa02e42
cauta <trongcauhcmus@gmail.com>
2024-05-05 10:32:23 +0700

    update supportedchain confirmation block

--- app/libs/utils/src/supportedChain.util.ts
+++ app/libs/utils/src/supportedChain.util.ts
@@ -10,55 +10,55 @@ export class SupportedChain {
     id: 56,
     idHex: '0x38',
     name: 'BSC',
-    confirmationBlock: 12,
+    confirmationBlock: 18,
   };
 
   static POLYGON = {
     id: 137,
     idHex: '0x89',
     name: 'POLYGON',
-    confirmationBlock: 100,
+    confirmationBlock: 50,
   };
 
   static AVALANCHE = {
     id: 43114,
     idHex: '0xa86a',
     name: 'AVALANCHE',
-    confirmationBlock: 100,
+    confirmationBlock: 50,
   };
 
   static FANTOM = {
     id: 250,
     idHex: '0xfa',
     name: 'FANTOM',
-    confirmationBlock: 100,
+    confirmationBlock: 50,
   };
 
   static ARBITRUM = {
     id: 42161,
     idHex: '0xa4b1',
     name: 'ARBITRUM',
-    confirmationBlock: 100,
+    confirmationBlock: 50,
   };
 
   static OPTIMISM = {
     id: 10,
     idHex: '0xa',
     name: 'OPTIMISM',
-    confirmationBlock: 100,
+    confirmationBlock: 50,
   };
 
   static BASE = {
     id: 8453,
     idHex: '0x2105',
     name: 'BASE',
-    confirmationBlock: 100,
+    confirmationBlock: 50,
   };
 
   static GNOSIS = {
     id: 100,
     idHex: '0x64',
     name: 'GNOSIS',
-    confirmationBlock: 100,
+    confirmationBlock: 50,
   };
 }
