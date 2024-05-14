26b1a6d67b9d626421c7cd57e7ae49488e491457
cauta <trongcauhcmus@gmail.com>
2024-05-14 15:56:13 +0700

    add warning when handler take too long

--- app/apps/monitor-service/src/ethereum/ethereum.controller.ts
+++ app/apps/monitor-service/src/ethereum/ethereum.controller.ts
@@ -1,24 +1,34 @@
 import { TopicName } from '@app/utils/topicUtils';
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 import { EthereumService } from './ethereum.service';
 
 @Controller()
 export class EthereumController {
+  private readonly logger = new Logger(EthereumController.name);
   constructor(private readonly ethereum: EthereumService) {}
 
   @EventPattern(TopicName.ETH_NATIVE_TRANSFER)
-  handleNativeTransfer(data: any): void {
-    this.ethereum.handleNativeTransfer(data);
+  async handleNativeTransfer(data: any): Promise<void> {
+    const start = Date.now();
+    await this.ethereum.handleNativeTransfer(data);
+    if (Date.now() - start > 20)
+      this.logger.warn(`Handle native transfer in : ${Date.now() - start}`);
   }
 
   @EventPattern(TopicName.ETH_ERC20_TRANSFER)
-  handleErc20Transfer(data: any): void {
-    this.ethereum.handleErc20Transfer(data);
+  async handleErc20Transfer(data: any): Promise<void> {
+    const start = Date.now();
+    await this.ethereum.handleErc20Transfer(data);
+    if (Date.now() - start > 20)
+      this.logger.warn(`Handle erc20 transfer in : ${Date.now() - start}`);
   }
 
   @EventPattern(TopicName.ETH_ERC721_TRANSFER)
-  handleErc721Transfer(data: any): void {
-    this.ethereum.handleErc721Transfer(data);
+  async handleErc721Transfer(data: any): Promise<void> {
+    const start = Date.now();
+    await this.ethereum.handleErc721Transfer(data);
+    if (Date.now() - start > 20)
+      this.logger.warn(`Handle erc721 transfer in : ${Date.now() - start}`);
   }
 }
