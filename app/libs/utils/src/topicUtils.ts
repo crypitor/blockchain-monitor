f00ecbf227a14cdf3e3319c07a0e0a57daa3ae6b
cauta <trongcauhcmus@gmail.com>
2024-05-14 15:27:12 +0700

    optimize worker and topic remove topic confirm block config handle block timeout 200ms

--- app/libs/utils/src/topicUtils.ts
+++ app/libs/utils/src/topicUtils.ts
@@ -1,12 +1,10 @@
 export enum TopicName {
   ETH_DETECTED_BLOCK = 'eth-detect-block',
-  ETH_CONFIRMED_BLOCK = 'eth-confirm-block',
   ETH_NATIVE_TRANSFER = 'eth-native-transfer',
   ETH_ERC20_TRANSFER = 'eth-erc20-transfer',
   ETH_ERC721_TRANSFER = 'eth-erc721-transfer',
 
   POLYGON_DETECTED_BLOCK = 'polygon-detect-block',
-  POLYGON_CONFIRMED_BLOCK = 'polygon-confirm-block',
   POLYGON_NATIVE_TRANSFER = 'polygon-native-transfer',
   POLYGON_ERC20_TRANSFER = 'polygon-erc20-transfer',
   POLYGON_ERC721_TRANSFER = 'polygon-erc721-transfer',
