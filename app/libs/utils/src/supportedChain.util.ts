e6b1da002e2e13c0b88f5a78931a18f8b92c6cd2
cauta <trongcauhcmus@gmail.com>
2024-05-16 23:13:03 +0700

    update supported chain

--- app/libs/utils/src/supportedChain.util.ts
+++ app/libs/utils/src/supportedChain.util.ts
@@ -2,8 +2,10 @@ export class SupportedChain {
   static ETH = {
     id: 1,
     idHex: '0x1',
-    name: 'ETH',
+    name: 'Ethereum',
     confirmationBlock: 12,
+    explorer: 'https://etherscan.io',
+    nativeCurrency: 'ETH',
   };
 
   static BSC = {
@@ -11,54 +13,79 @@ export class SupportedChain {
     idHex: '0x38',
     name: 'BSC',
     confirmationBlock: 18,
+    explorer: 'https://bscscan.com',
+    nativeCurrency: 'BNB',
   };
 
   static POLYGON = {
     id: 137,
     idHex: '0x89',
-    name: 'POLYGON',
+    name: 'Polygon',
     confirmationBlock: 50,
+    explorer: 'https://polygonscan.com',
+    nativeCurrency: 'MATIC',
   };
 
   static AVALANCHE = {
     id: 43114,
     idHex: '0xa86a',
-    name: 'AVALANCHE',
+    name: 'Avalanche',
     confirmationBlock: 50,
-  };
-
-  static FANTOM = {
-    id: 250,
-    idHex: '0xfa',
-    name: 'FANTOM',
-    confirmationBlock: 50,
+    explorer: 'https://snowtrace.io',
+    nativeCurrency: 'AVAX',
   };
 
   static ARBITRUM = {
     id: 42161,
     idHex: '0xa4b1',
-    name: 'ARBITRUM',
+    name: 'Arbitrum',
     confirmationBlock: 50,
-  };
-
-  static OPTIMISM = {
-    id: 10,
-    idHex: '0xa',
-    name: 'OPTIMISM',
-    confirmationBlock: 50,
+    explorer: 'https://arbiscan.io',
+    nativeCurrency: 'ETH',
   };
 
   static BASE = {
     id: 8453,
     idHex: '0x2105',
-    name: 'BASE',
+    name: 'Base',
     confirmationBlock: 50,
+    explorer: 'https://basescan.org/',
+    nativeCurrency: 'BASE',
+  };
+
+  static MANTLE = {
+    id: 5000,
+    idHex: '0x1388',
+    name: 'Mantle',
+    confirmationBlock: 50,
+    explorer: 'https://explorer.mantle.xyz',
+    nativeCurrency: 'MNT',
+  };
+
+  static FANTOM = {
+    id: 250,
+    idHex: '0xfa',
+    name: 'Fantom',
+    confirmationBlock: 50,
+    explorer: 'https://ftmscan.com',
+    nativeCurrency: 'FTM',
+  };
+
+  static OPTIMISM = {
+    id: 10,
+    idHex: '0xa',
+    name: 'Optimism',
+    confirmationBlock: 50,
+    explorer: 'https://optimistic.etherscan.io',
+    nativeCurrency: 'ETH',
   };
 
   static GNOSIS = {
     id: 100,
     idHex: '0x64',
-    name: 'GNOSIS',
+    name: 'Gnosis',
     confirmationBlock: 50,
+    explorer: 'https://gnosisscan.io',
+    nativeCurrency: 'xDAI',
   };
 }
