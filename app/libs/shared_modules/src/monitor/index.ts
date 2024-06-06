25b46ec1805b4653bcfe481bc7b5e704ef9c453d
cauta <trongcauhcmus@gmail.com>
2024-06-06 14:22:39 +0700

    fix export module

--- app/libs/shared_modules/src/monitor/index.ts
+++ app/libs/shared_modules/src/monitor/index.ts
@@ -2,5 +2,6 @@ export * from './monitor.module';
 export * from './monitor.provider';
 export * from './services/monitor.webhook.service';
 export * from './repositories/monitor.repository';
+export * from './repositories/monitor.address.repository';
 export * from './schemas/monitor.schema';
 export * from './schemas/monitor.address.schema';
