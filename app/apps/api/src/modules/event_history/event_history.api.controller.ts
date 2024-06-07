8a882805c6139cfc02613ff6e389850b0f27680f
cauta <trongcauhcmus@gmail.com>
2024-06-07 15:16:15 +0700

    add event history api module

---
+++ app/apps/api/src/modules/event_history/event_history.api.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Query, Req } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import {
+  GetMonitorEventHistoryByAssociatedAddressDto,
+  GetMonitorEventHistoryByEventIdDto,
+  GetMonitorEventHistoryByHashDto,
+  GetMonitorEventHistoryDto,
+  MonitorEventHistoryResponseDto,
+} from './dto/event_history.api.dto';
+import { EventHistoryService } from './event_history.api.service';
+
+@ApiTags('Monitor Event History')
+@Controller('/event')
+export class EventHistoryController {
+  constructor(private readonly eventHistoryService: EventHistoryService) {}
+
+  @ApiOperation({ summary: 'Get Monitor Event' })
+  @Get('')
+  @ApiOkResponse({ type: [MonitorEventHistoryResponseDto] })
+  async getMonitorEvent(
+    @Req() req: Request,
+    @Query() body: GetMonitorEventHistoryDto,
+  ): Promise<MonitorEventHistoryResponseDto[]> {
+    return await this.eventHistoryService.getMonitorEventHistory(body);
+  }
+
+  @ApiOperation({ summary: 'Get Monitor Event History By Transaction Hash' })
+  @Get('by-hash')
+  @ApiOkResponse({ type: [MonitorEventHistoryResponseDto] })
+  async getMonitorEventHistoryByTxnHash(
+    @Req() req: Request,
+    @Query() body: GetMonitorEventHistoryByHashDto,
+  ): Promise<MonitorEventHistoryResponseDto[]> {
+    return await this.eventHistoryService.getMonitorEventHistoryByTxnHash(body);
+  }
+
+  @ApiOperation({ summary: 'Get Monitor Event History By Event Id' })
+  @Get('by-event-id')
+  @ApiOkResponse({ type: [MonitorEventHistoryResponseDto] })
+  async getMonitorEventHistoryByEventId(
+    @Req() req: Request,
+    @Query() body: GetMonitorEventHistoryByEventIdDto,
+  ): Promise<MonitorEventHistoryResponseDto> {
+    return await this.eventHistoryService.getMonitorEventHistoryByEventId(body);
+  }
+
+  @ApiOperation({ summary: 'Get Monitor Event History By Associated Address' })
+  @Get('by-address')
+  @ApiOkResponse({ type: [MonitorEventHistoryResponseDto] })
+  async getMonitorEventHistoryByAssociatedAddress(
+    @Req() req: Request,
+    @Query() body: GetMonitorEventHistoryByAssociatedAddressDto,
+  ): Promise<MonitorEventHistoryResponseDto[]> {
+    return await this.eventHistoryService.getMonitorEventHistoryByAssociatedAddress(
+      body,
+    );
+  }
+}
---
+++ app/apps/api/src/modules/event_history/event_history.api.service.ts
@@ -0,0 +1,317 @@
+import { ErrorCode } from '@app/global/global.error';
+import {
+  AvaxEventHistoryRepository,
+  BscEventHistoryRepository,
+  EthEventHistoryRepository,
+  MantleEventHistoryRepository,
+  PolygonEventHistoryRepository,
+} from '@app/shared_modules/event_history/repositories/event_history.repository';
+import { MonitorNetwork } from '@app/shared_modules/monitor/schemas/monitor.schema';
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  GetMonitorEventHistoryByAssociatedAddressDto,
+  GetMonitorEventHistoryByEventIdDto,
+  GetMonitorEventHistoryByHashDto,
+  GetMonitorEventHistoryDto,
+  MonitorEventHistoryResponseDto,
+} from './dto/event_history.api.dto';
+import { MonitorApiService } from '../monitor/monitor.api.service';
+
+@Injectable()
+export class EventHistoryService {
+  private readonly logger = new Logger(EventHistoryService.name);
+  constructor(
+    private readonly ethEventHistoryRepository: EthEventHistoryRepository,
+    private readonly polygonEventHistoryRepository: PolygonEventHistoryRepository,
+    private readonly avaxEventHistoryRepository: AvaxEventHistoryRepository,
+    private readonly mantleEventHistoryRepository: MantleEventHistoryRepository,
+    private readonly bscEventHistoryRepository: BscEventHistoryRepository,
+    private readonly monitorApiService: MonitorApiService,
+  ) {}
+
+  async getMonitorEventHistory(
+    request: GetMonitorEventHistoryDto,
+  ): Promise<MonitorEventHistoryResponseDto[]> {
+    const monitor = await this.monitorApiService.findMonitor(request.monitorId);
+
+    if (!monitor) {
+      throw ErrorCode.MONITOR_NOT_FOUND.asException();
+    }
+
+    if (monitor.network === MonitorNetwork.Ethereum) {
+      return this.ethEventHistoryRepository
+        .getEventHistory(monitor.monitorId, request.limit, request.offset)
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Polygon) {
+      return this.polygonEventHistoryRepository
+        .getEventHistory(monitor.monitorId, request.limit, request.offset)
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Avalanche) {
+      return this.avaxEventHistoryRepository
+        .getEventHistory(monitor.monitorId, request.limit, request.offset)
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Mantle) {
+      return this.mantleEventHistoryRepository
+        .getEventHistory(monitor.monitorId, request.limit, request.offset)
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.BSC) {
+      return this.bscEventHistoryRepository
+        .getEventHistory(monitor.monitorId, request.limit, request.offset)
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    this.logger.error(`network ${monitor.network} not supported`);
+    throw ErrorCode.INTERNAL_SERVER_ERROR.asException();
+  }
+
+  async getMonitorEventHistoryByTxnHash(
+    request: GetMonitorEventHistoryByHashDto,
+  ): Promise<MonitorEventHistoryResponseDto[]> {
+    const monitor = await this.monitorApiService.findMonitor(request.monitorId);
+
+    if (!monitor) {
+      throw ErrorCode.MONITOR_NOT_FOUND.asException();
+    }
+    if (monitor.network === MonitorNetwork.Ethereum) {
+      return this.ethEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.hash,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+    if (monitor.network === MonitorNetwork.Polygon) {
+      return this.polygonEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.hash,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Avalanche) {
+      return this.avaxEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.hash,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Mantle) {
+      return this.mantleEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.hash,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.BSC) {
+      return this.bscEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.hash,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    this.logger.error(`network ${monitor.network} not supported`);
+    throw ErrorCode.INTERNAL_SERVER_ERROR.asException();
+  }
+
+  async getMonitorEventHistoryByAssociatedAddress(
+    request: GetMonitorEventHistoryByAssociatedAddressDto,
+  ): Promise<MonitorEventHistoryResponseDto[]> {
+    const monitor = await this.monitorApiService.findMonitor(request.monitorId);
+
+    if (!monitor) {
+      throw ErrorCode.MONITOR_NOT_FOUND.asException();
+    }
+    if (monitor.network === MonitorNetwork.Ethereum) {
+      return this.ethEventHistoryRepository
+        .findByMonitorAndAssociatedAddress(
+          monitor.monitorId,
+          request.associatedAddress,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+    if (monitor.network === MonitorNetwork.Polygon) {
+      return this.polygonEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.associatedAddress,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Avalanche) {
+      return this.avaxEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.associatedAddress,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Mantle) {
+      return this.mantleEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.associatedAddress,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.BSC) {
+      return this.bscEventHistoryRepository
+        .findEventHistoryByMonitorAndHash(
+          monitor.monitorId,
+          request.associatedAddress,
+          request.limit,
+          request.offset,
+        )
+        .then((response) => {
+          return response.map((event) =>
+            MonitorEventHistoryResponseDto.from(event),
+          );
+        });
+    }
+
+    this.logger.error(`network ${monitor.network} not supported`);
+    throw ErrorCode.INTERNAL_SERVER_ERROR.asException();
+  }
+
+  async getMonitorEventHistoryByEventId(
+    request: GetMonitorEventHistoryByEventIdDto,
+  ): Promise<MonitorEventHistoryResponseDto> {
+    const monitor = await this.monitorApiService.findMonitor(request.monitorId);
+
+    if (!monitor) {
+      throw ErrorCode.MONITOR_NOT_FOUND.asException();
+    }
+    if (monitor.network === MonitorNetwork.Ethereum) {
+      return this.ethEventHistoryRepository
+        .findByEventId(request.eventId)
+        .then((response) => {
+          return MonitorEventHistoryResponseDto.from(response);
+        });
+    }
+    if (monitor.network === MonitorNetwork.Polygon) {
+      return this.polygonEventHistoryRepository
+        .findByEventId(request.eventId)
+        .then((response) => {
+          return MonitorEventHistoryResponseDto.from(response);
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Avalanche) {
+      return this.avaxEventHistoryRepository
+        .findByEventId(request.eventId)
+        .then((response) => {
+          return MonitorEventHistoryResponseDto.from(response);
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.Mantle) {
+      return this.mantleEventHistoryRepository
+        .findByEventId(request.eventId)
+        .then((response) => {
+          return MonitorEventHistoryResponseDto.from(response);
+        });
+    }
+
+    if (monitor.network === MonitorNetwork.BSC) {
+      return this.bscEventHistoryRepository
+        .findByEventId(request.eventId)
+        .then((response) => {
+          return MonitorEventHistoryResponseDto.from(response);
+        });
+    }
+
+    this.logger.error(`network ${monitor.network} not supported`);
+    throw ErrorCode.INTERNAL_SERVER_ERROR.asException();
+  }
+}
